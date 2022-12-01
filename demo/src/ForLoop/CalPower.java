95a3562e6184fe1894e74fb79f0306c3c0330395
ROHIT <ROHIT@Oprekin-PC>
2022-12-01 08:03:06 -0800

    Practice Project

---
+++ demo/src/ForLoop/CalPower.java
@@ -0,0 +1,23 @@
+package ForLoop;
+
+import java.util.Scanner;
+
+public class CalPower {
+public static void main(String[] args) {
+		
+		int b,e,pow=1 ;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the base and exponent value : ");
+		b=sc.nextInt();
+		
+		e=sc.nextInt();
+		
+
+		for(int i=1;i<=e;i++)
+		{
+			pow=pow*b;
+		}
+		System.out.println("power of " +pow);
+		sc.close();
+}
+}
